4da9992e334cda1ee694a27cbaa9fa36cfb4b502
tamzi <tamrefrank@gmail.com>
2022-05-28 01:27:10 +0300

    init with ktor2.2

---
+++ src/main/kotlin/com/tamzi/plugins/Security.kt
@@ -0,0 +1,97 @@
+package com.tamzi.plugins
+
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.auth.ldap.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Application.configureSecurity() {
+
+    authentication {
+        basic(name = "myauth1") {
+            realm = "Ktor Server"
+            validate { credentials ->
+                if (credentials.name == credentials.password) {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
+
+        form(name = "myauth2") {
+            userParamName = "user"
+            passwordParamName = "password"
+            challenge {
+                /**/
+            }
+        }
+    }
+    authentication {
+        val myRealm = "MyRealm"
+        val usersInMyRealmToHA1: Map<String, ByteArray> = mapOf(
+                // pass="test", HA1=MD5("test:MyRealm:pass")="fb12475e62dedc5c2744d98eb73b8877"
+                "test" to hex("fb12475e62dedc5c2744d98eb73b8877")
+        )
+
+        digest("myDigestAuth") {
+            digestProvider { userName, realm ->
+                usersInMyRealmToHA1[userName]
+            }
+        }
+    }
+    authentication {
+        jwt {
+            val jwtAudience = this@configureSecurity.environment.config.property("jwt.audience").getString()
+            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
+            verifier(
+                    JWT
+                            .require(Algorithm.HMAC256("secret"))
+                            .withAudience(jwtAudience)
+                            .withIssuer(this@configureSecurity.environment.config.property("jwt.domain").getString())
+                            .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+    val localhost = "http://0.0.0.0"
+    val ldapServerPort = 6998 // TODO: change to real value!
+    authentication {
+        basic("authName") {
+            realm = "realm"
+            validate { credential ->
+                ldapAuthenticate(credential, "ldap://$localhost:${ldapServerPort}", "uid=%s,ou=system")
+            }
+        }
+    }
+
+    routing {
+        authenticate("myauth1") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+        authenticate("myauth1") {
+            get("/protected/route/form") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+        authenticate("myDigestAuth") {
+            get("/protected/route/digest") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+    }
+}
